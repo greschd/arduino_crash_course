bf2ca2ef39b649cab42b3bea614835db4f983ccb
mskoenz <mskoenz@gmx.net>
2013-11-13 15:38:13 +0100

    Test commit rights

--- program/tetris/tetris.cpp
+++ program/tetris/tetris.cpp
@@ -8,6 +8,8 @@
  * To Public License, Version 2, as published by Sam Hocevar. See
  * http://www.wtfpl.net/ or COPYING for more details. */
 
+//Baus
+
 #include <Arduino.h>
 #include <ustd.hpp>
 #include <tool.hpp>
